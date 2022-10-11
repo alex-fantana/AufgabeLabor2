2b1878cf9de4fed540aadf124606837d080d217f
alex-fantana <52429567+alex-fantana@users.noreply.github.com>
2022-10-11 11:30:59 +0300

    Array of object

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,29 @@ import MVC.CarsView;
 
 public class Main {
     public static void main(String[] args) {
-        Cars model = retriveCars();
+        Cars[] model = new Cars[2];
+        model[0] = retriveCars();
         CarsView view = new CarsView();
-        CarsController controller = new CarsController(model,view);
+        CarsController controller = new CarsController(model[0],view);
         controller.updateView();
         controller.setModelName("Spring");
         controller.updateView();
+        model[1] = retriveCars2();
+        CarsController controller2 = new CarsController(model[1],view);
+        controller2.updateView();
     }
     private static Cars retriveCars(){
         Cars model = new Cars();
         model.setModel("Logan");
         model.setFirm("Dacia");
         model.setID(1);
+        return model;
+    }
+    private static Cars retriveCars2(){
+        Cars model = new Cars();
+        model.setModel("Papuc");
+        model.setFirm("Dacia");
+        model.setID(2);
         return model;
     }
 }
